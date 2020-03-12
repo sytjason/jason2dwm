fb2633359e85c2d20059091bbbb92d36fa847d0e
sytjason <jasont1020508@gmail.com>
2020-03-12 17:26:11 +0800

    systray update

--- config.h
+++ config.h
@@ -7,6 +7,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "DejaVuSansMono Nerd Font:size=10" };
 static const char dmenufont[]       = "DejaVuSansMono Nerd Font:size=10";
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
