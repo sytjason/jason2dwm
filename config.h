d650c7243abb0bbec6ac8428b449ec04450a8217
sytjason <jasont1020508@gmail.com>
2020-03-14 20:20:14 +0800

    screen light updated

--- config.h
+++ config.h
@@ -1,3 +1,4 @@
+#include <X11/XF86keysym.h>
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
@@ -64,10 +65,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+static const char *upbright[] = { "xbacklight", "-inc", "1", NULL };
+static const char *downbright[] = { "xbacklight", "-dec", "1", NULL };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+    { 0,              XF86XK_MonBrightnessUp,  spawn,          {.v = upbright } },
+    { 0,              XF86XK_MonBrightnessDown,spawn,          {.v = downbright } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
