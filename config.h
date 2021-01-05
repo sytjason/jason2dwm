8da9cabc5854acedafecc5293a8d6a415d52534a
sytjason <jasont1020508@gmail.com>
2021-01-05 16:26:26 +0800

    default terminal update kitty

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "terminator", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static const char *upbright[] = { "xbacklight", "-inc", "1", NULL };
 static const char *downbright[] = { "xbacklight", "-dec", "1", NULL };
